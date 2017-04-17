94616183d13cc212cae8c203fb91f37b1ed1b5c7
Gabriel Brito <gabriel.brito55@gmail.com>
2017-04-17 14:45:16 -0300

    hello gabriel

--- src/gitTest/Testes.java
+++ src/gitTest/Testes.java
@@ -3,7 +3,6 @@ package gitTest;
 public class Testes {
 	public static void main(String args[]){
 		System.out.println("Hello World");
-		System.out.println("Hello Git");
-		System.out.println("Hello Hub");
+		System.out.println("Gabriel");
 	}
 }
